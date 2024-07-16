9fd33d359820dd44f2c9d4070ca92baf1ef0b196
Максим Мельников <m.melnikov@idaproject.com>
2024-07-16 10:43:13 +0300

    сквош миграция

--- src/apps/developers/models.py
+++ src/apps/developers/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Developer(models.Model):
     title = models.CharField("Артикул", max_length=32)
     inn = models.CharField(verbose_name="ИНН", max_length=12, blank=True)
+    rating = models.FloatField(verbose_name="Рейтинг", default=0.0)
 
     class Meta:
         indexes = [models.Index(fields=["title"])]
