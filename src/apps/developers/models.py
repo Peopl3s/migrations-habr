f083177e6cd232f5fd28b6bd382fdd33bf39ac7b
Максим Мельников <m.melnikov@idaproject.com>
2024-07-16 08:28:10 +0300

    добавил индекс на поле title и миграцию

--- src/apps/developers/models.py
+++ src/apps/developers/models.py
@@ -5,5 +5,6 @@ class Developer(models.Model):
     title = models.CharField("Артикул", max_length=32)
 
     class Meta:
+        indexes = [models.Index(fields=["title"])]
         verbose_name = "Застройщик"
         verbose_name_plural = "Застройщики"
