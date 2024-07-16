e8e8b48985e212fc5dad397012c2e433449e20f5
Максим Мельников <m.melnikov@idaproject.com>
2024-07-16 08:17:06 +0300

    добавил поле инн и миграция

--- src/apps/developers/models.py
+++ src/apps/developers/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Developer(models.Model):
     title = models.CharField("Артикул", max_length=32)
+    inn = models.CharField(verbose_name="ИНН", max_length=12, blank=True)
 
     class Meta:
         verbose_name = "Застройщик"
